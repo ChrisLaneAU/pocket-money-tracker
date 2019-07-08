7d39763f8290d796f9967614e5215f1f1935d08b
ChrisLaneAU <chrislane10001@gmail.com>
2019-07-09 09:22:54 +1000

    refactor ViewPanelDashboard into own file

--- components/ViewPanel/ViewPanelDashboard/ViewPanelDashboard.tsx
+++ components/ViewPanel/ViewPanelDashboard/ViewPanelDashboard.tsx
@@ -2,11 +2,9 @@ import * as React from "react";
 import { useState } from "react";
 import "./ViewPanel.scss";
 
-import BackButton from "../BackButton/BackButton";
 import MainImage from "../MainImage/MainImage";
 import ActionButton from "../ActionButton/ActionButton";
 import Name from "../Name/Name";
-import ProgressIndicator from "../ProgressIndicator/ProgressIndicator";
 import ModalWindow from "../../ModalWindow/ModalWindow";
 import Form from "../../Form/Form";
 
@@ -49,7 +47,6 @@ type Goal = {
 const ViewPanel = ({ user, currentPage, currentGoal, buttonsData }: Props) => {
   const [showModal, setShowModal] = useState(false);
   const [activeForm, setActiveForm] = useState("newGoal");
-  const [progressVal, setProgressVal] = useState(currentGoal.progress);
 
   const showForm = (show: boolean, label: string) => {
     if (!label.includes("New")) return;
@@ -59,46 +56,6 @@ const ViewPanel = ({ user, currentPage, currentGoal, buttonsData }: Props) => {
   };
 
   // const showChores = choresList => {};
-
-  const updateProgress = async choreValue => {
-    const { id, name, image, child, price, progress } = currentGoal;
-    const newProgress = Number(progress) + Number(choreValue);
-
-    setProgressVal(newProgress);
-
-    const query = `
-    mutation {
-       updateGoal(id:"${id}", name: "${name}", image: "${image}", child: "${child}", price: "${price}", progress: "${newProgress}") {
-          id,
-          name,
-          image,
-          child,
-          price,
-          progress
-      }
-    }
-    `;
-
-    // const url = "http://localhost:3001/graphql";
-    const url = "https://pocket-money-tracker-api.herokuapp.com/graphql";
-
-    const options: any = {
-      method: "POST",
-      mode: "cors",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query })
-    };
-
-    const res = await fetch(url, options);
-    const data = await res.json();
-
-    return {
-      currentGoal: data.data.goal
-    };
-  };
-
-  const handleButtonClick =
-    currentPage === "dashboard" ? showForm : updateProgress;
 
   const renderButtons = () => {
     // TODO: refactor to avoid using variable a
@@ -115,70 +72,28 @@ const ViewPanel = ({ user, currentPage, currentGoal, buttonsData }: Props) => {
           currentPage={currentPage}
           buttonsData={{ ...buttonsData, buttonClasses, containerClass }}
           showForm={showForm}
-          handleButtonClick={handleButtonClick}
+          handleButtonClick={showForm}
         />
       );
     }).filter(content => content !== undefined);
   };
-
-  const renderProgressIndicators = () => {
-    const { price } = currentGoal;
-
-    return Array.from(Array(Number(price)), (val, index) => {
-      return (
-        <ProgressIndicator
-          key={`progress-${index}-${val}`}
-          isEarned={index < Number(progressVal)}
-        />
-      );
-    });
-  };
-
-  const progress: React.ReactNode =
-    currentPage == "dashboard" ? (
-      <></>
-    ) : (
-      <>
-        <h2 className="view-panel-progress-heading">
-          <span className="view-panel-progress-heading-span">Progress:</span>
-          {` $${progressVal} / $${currentGoal.price}`}
-        </h2>
-        <ul className="view-panel-progress-list">
-          {Object.values(currentGoal).length ? (
-            renderProgressIndicators()
-          ) : (
-            <></>
-          )}
-        </ul>
-      </>
-    );
-
-  const backButton: React.ReactNode =
-    currentPage == "dashboard" ? <></> : <BackButton />;
 
   const pageData = currentPage == "dashboard" ? user : currentGoal;
   const { image, name } = pageData;
 
   const listClasses = `view-panel-action-button-list view-panel-action-button-list-${currentPage}`;
 
-  const modalWindow: React.ReactNode =
-    currentPage == "dashboard" ? (
-      <ModalWindow showForm={showForm} heading={forms[activeForm].heading}>
-        <Form currentPage={currentPage} inputs={forms[activeForm].inputs} />
-      </ModalWindow>
-    ) : (
-      <ModalWindow showForm={showForm} heading={forms[activeForm].heading}>
-        <Form currentPage={currentPage} inputs={forms[activeForm].inputs} />
-      </ModalWindow>
-    );
+  const modalWindow: React.ReactNode = (
+    <ModalWindow showForm={showForm} heading={forms[activeForm].heading}>
+      <Form currentPage={currentPage} inputs={forms[activeForm].inputs} />
+    </ModalWindow>
+  );
 
   return (
     <section className="view-panel">
-      {backButton}
       <MainImage image={image} description={name} currentPage={currentPage} />
       <ul className={listClasses}>{renderButtons()}</ul>
       <Name currentPage={currentPage} name={name} />
-      {progress}
       {showModal ? modalWindow : <></>}
     </section>
   );
